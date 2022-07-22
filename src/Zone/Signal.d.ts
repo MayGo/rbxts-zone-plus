6361923868d90dcf216ff6a79e796d8c6f0ed4b7
MayGo <maigo.erit@gmail.com>
2022-07-22 11:43:07 +0300

    Replace Zone v2 with v3 https://github.com/1ForeverHD/ZonePlus/commit/5c6c0e58256fefb891669420e807466657eeb1a7

--- src/Zone/Signal.d.ts
+++
@@ -1,9 +0,0 @@
-interface Signal<T extends Callback = Callback> {
-	Fire(...args: Parameters<T>): void;
-	Connect(handler: T): RBXScriptConnection;
-	Wait(): LuaTuple<Parameters<T>>;
-	Destroy(): void;
-	Disconnect(): void;
-}
-
-export = Signal;
